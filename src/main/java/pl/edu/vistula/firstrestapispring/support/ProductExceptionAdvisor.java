5f8c06053d1c348e4c404b4461c0c2400c4a7aaa
Ayberfin <aerdem1@stu.vistula.edu.pl>
2025-12-26 05:01:31 +0100

    Task 2 completed

---
+++ src/main/java/pl/edu/vistula/firstrestapispring/support/ProductExceptionAdvisor.java
@@ -0,0 +1,27 @@
+package pl.edu.vistula.firstrestapispring.support;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import pl.edu.vistula.firstrestapispring.shared.api.response.ErrorMessageResponse;
+import pl.edu.vistula.firstrestapispring.support.exception.ProductNotFoundException;
+
+
+@ControllerAdvice
+public class ProductExceptionAdvisor {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductExceptionAdvisor.class);
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorMessageResponse productNotFound(Exception e){
+        log.error(e.getMessage(), e);
+        return new ErrorMessageResponse(e.getLocalizedMessage());
+    }
+}
---
+++ src/main/java/pl/edu/vistula/firstrestapispring/support/ProductExceptionSupplier.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.firstrestapispring.support;
+
+import pl.edu.vistula.firstrestapispring.product.api.response.ProductResponse;
+import pl.edu.vistula.firstrestapispring.support.exception.ProductNotFoundException;
+
+import java.util.function.Supplier;
+
+public class ProductExceptionSupplier {
+
+    public static Supplier<ProductNotFoundException> productNotFound(Long id){
+        return () -> new ProductNotFoundException(id);
+    }
+}
